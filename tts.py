99fd321f8a847aad3118d311e26165d9118ec2dd
Programming Error <75001577+ProgrammingError@users.noreply.github.com>
2021-02-11 15:34:35 +0530

    Update tts.py

--- tts.py
+++ tts.py
@@ -1,12 +1,8 @@
-""" Google Text to Speech
-Available Commands:
-.tts LanguageCode as reply to a message
-.tts LangaugeCode | text to speak"""
 from dotenv import load_dotenv
 
 from pathlib import Path
-env_path = Path('.') / 'config.env'
-load_dotenv(dotenv_path=env_path)
+"""env_path = Path('.') / 'config.env'
+load_dotenv(dotenv_path=env_path)"""
 
 import asyncio
 import os, sys
